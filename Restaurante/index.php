c0181b547a975500dbae43de59e6df475898fcca
RSP-L3-LT-003 <RSP-L3-LT-003@UMD.LOCAL.EDU>
2025-02-20 17:50:58 -0500

    Hola mundo

--- Restaurante/index.php
+++ Restaurante/index.php
@@ -8,13 +8,42 @@
 </head>
 <body>
     <header class="header">
-
+        <nav class="nav">
+            <h1>Restaurante</h1>
+            <a href="#">Inicio</a>
+            <a href="#">Cheff</a>
+            <a href="#">Platos</a>
+            <a href="#">Testimonios</a>
+            <a href="#">Contacto</a>
+        </nav>
+        <div class="banner">
+            <h1>Restaurante Laura</h1>
+            <p>El mejor restaurante</p>
+            <button>Ver Menu</button>
+        </div>
     </header>
     <main class="main">
-
+        <section class="anucio">
+            <h2>Bienvenido al restaurante</h2>
+            <p>Los mejores platos de la ciudad</p>
+        </section>
+        <section class="chef">
+            <div class="chef1">
+                <img src="/images/chef/pexels-nadin-sh-78971847-29160624.jpg" alt="">
+                <p>Nami</p>
+            </div>
+            <div class="chef1">
+                <img src="/images/chef/pexels-miquel-ferran-gomez-figueroa-2172703-3814446.jpg" alt="">
+                <p>Carlos</p>
+            </div>
+            <div class="chef1">
+                <img src="/images/chef/pexels-monleo-yellow-chef-1313698478-24876033.jpg" alt="">
+                <p>yAMI</p>
+            </div>
+        </section>
     </main>
     <footer class="footer">
-
+        <p>copy universidad 2025</p>
     </footer>
 </body>
 </html>
